cf852022d0bccff181256df3616b12af62bba2ae
Jatin Gupta <114533828+Jatingupta9120@users.noreply.github.com>
2023-12-28 18:49:00 +0530

    Java Solution
            Submission Statistics:
            Question #: 345
            Language: Java
            Runtime: 5 ms
            Runtime percentile: 41.60
            Memory: 44.544 MB
            Memory percentile: 29.15

---
+++ 345. Reverse Vowels of a String/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String reverseVowels(String s) {
+        char word[]=s.toCharArray();
+        int i=0;
+        int j=s.length()-1;
+        String vowel="aeiouAEIOU";
+        while(i<j){
+          while(i<j&&vowel.indexOf(word[i])==-1){
+              i++;
+          }  
+          while(i<j&&vowel.indexOf(word[j])==-1){
+              j--;
+
+          }
+          char temp=word[i];
+          word[i]=word[j];
+          word[j]=temp;
+          i++;
+          j--;
+
+        }
+          String ans=new String (word);
+          return ans;
+    }
+}
