970dcd6bc92eec7ff44cf97414a1f2661ba6f359
Jatin Gupta <114533828+Jatingupta9120@users.noreply.github.com>
2024-06-08 01:28:49 +0530

    Time: 63 ms (26.81%) | Memory: 45.9 MB (14.50%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,27 +1,26 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-      if (hand.length % groupSize != 0) {
-            return false;
+        int n=hand.length;
+        if(n%groupSize!=0)return false;//checkone for if groupsize%n!=0 return false;
+        TreeMap<Integer,Integer>tm=new TreeMap<>();//we are creating a map and storing all values,freq in sorted order
+        for(int i:hand){
+            tm.put(i,tm.getOrDefault(i,0)+1);
         }
-
-        Map<Integer, Integer> cardCount = new TreeMap<>();
-        for (int card : hand) {
-            cardCount.put(card, cardCount.getOrDefault(card, 0) + 1);
-        }
-
-        for (int card : cardCount.keySet()) {
-            int count = cardCount.get(card);
-            if (count > 0) {
-                for (int i = 0; i < groupSize; ++i) {
-                    if (cardCount.getOrDefault(card + i, 0) < count) {
-                        return false;
-                    }
-                    cardCount.put(card + i, cardCount.get(card + i) - count);
+        while(!tm.isEmpty()){//we are taking values from map
+            
+            int curr=tm.firstKey();//getting 1st element from map 
+            for(int i=0;i<groupSize;i++){//loop for groupsize like curr=1 and so remaining 2(curr+i) checkif it exist or not 
+                int val=curr+i;
+                if(!tm.containsKey(val)){
+                   return false;// if not return false;
                 }
+                tm.put(val,tm.get(val)-1);
+                if(tm.get(val)==0){
+                    tm.remove(val);
+                };
             }
         }
-
         return true;
-    }   
-    
+        
+    }
 }
