4c950a2c4672bff7468823a91d3cf1c4fe84a13a
Jatin Gupta <114533828+Jatingupta9120@users.noreply.github.com>
2025-09-23 01:36:17 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (10.04%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -14,21 +14,32 @@ class Solution {
         if (head == null || head.next == null || k == 0) return head;
     
         ListNode temp=head;
-        int len_node=1;
-        while(temp.next!=null){
+        int len_node=0;
+        while(temp!=null){
             temp=temp.next;
             len_node++;
         }
         //make it circular and store front values after this and break after this and connect with front head
-        temp.next=head;
         k = k % len_node;
+        if(k == 0 ) {
+            return head;
+        }
         int stepsToNewTail = len_node - k;
         ListNode newTail = head;
         for(int i=1;i<stepsToNewTail;i++){
             newTail=newTail.next;
         }
+
         ListNode newHead=newTail.next;
         newTail.next=null;
+
+        ListNode prevNode = new ListNode(-1);
+        temp = newHead;
+        while(temp != null) {
+            prevNode = temp;
+            temp= temp.next;
+        }
+        prevNode.next = head;
         return newHead;
        
     }
